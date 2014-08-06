1f732fbeb255f6d49796f351a2fc7402d8447c35
Jesse Weisner <jesse_weisner@bcit.ca>
2014-08-06 12:40:13 -0700

    first attempt

---
+++ manifests/repo.pp
@@ -0,0 +1,26 @@
+define repo(
+    $basedir,
+    $owner,
+    $group,
+    $mode,
+) {
+    file { "${basedir}/$name":
+        ensure  => directory,
+        owner   => $owner,
+        group   => $group,
+        mode    => $mode,
+        require => File[$basedir]
+    }
+
+    exec { "Create repo ${basedir}/${name}":
+        command => "/usr/bin/createrepo .",
+        creates => "${basedir}/${name}/repodata",
+        cwd     => "${basedir}/${name}",
+        group   => $group,
+        user    => $owner,
+        require => [
+            File["${basedir}/${name}"],
+            Package["createrepo"],
+        ],
+    }
+}
---
+++ manifests/server.pp
@@ -0,0 +1,13 @@
+class yum::server(
+    $basedir = '/var/www/yum',
+    $repos   = [],
+) inherits yum {
+    file { $basedir:
+        ensure => directory,
+        owner  => $owner,
+        group  => $group,
+        mode   => $mode
+    }
+
+    yum::repo{$repos}
+}
