f80c622ff3442655134d0a42b14c975f13e0c3a9
Jesse Weisner <jesse_weisner@bcit.ca>
2014-08-06 11:47:39 -0700

    added boilerplate module poop

---
+++ manifests/init.pp
@@ -0,0 +1,42 @@
+# == Class: yum
+#
+# Yum configuration management
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if it
+#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should not be used in preference to class parameters  as of
+#   Puppet 2.6.)
+#
+# === Examples
+#
+#  class { yum:
+#  }
+#
+# === Authors
+#
+# Author Name <Jesse Weisner@bcit.ca>
+#
+# === Copyright
+#
+# Copyright 2014 Jesse Weisner
+#
+
+class yum {
+    #
+    # This is an empty class
+    #
+}
