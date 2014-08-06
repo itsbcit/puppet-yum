492cdad878f332455078091a08b7de98c2572a83
Jesse Weisner <jesse_weisner@bcit.ca>
2014-08-06 13:35:16 -0700

    left out the params

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,6 +1,9 @@
 class yum::server(
     $basedir = '/var/www/yum',
     $repos   = [],
+    $owner   = 'root',
+    $group   = 'root',
+    $mode    = '0755'
 ) inherits yum {
     file { $basedir:
         ensure => directory,
@@ -9,5 +12,9 @@ class yum::server(
         mode   => $mode
     }
 
-    yum::repo{$repos: }
+    yum::repo{$repos:
+        owner  => $owner,
+        group  => $group,
+        mode   => $mode
+    }
 }
