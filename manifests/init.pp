bdfacd566cf690e30b610c271f9994e339491048
Jesse Weisner <jesse@weisner.ca>
2014-11-02 22:44:12 -0800

    try removing the namespace anchors

--- manifests/init.pp
+++ manifests/init.pp
@@ -59,7 +59,7 @@ class yum (
   # begin options processing
 
   # merge together all default_repos sources
-  $default_default_repos = getvar("::yum::os::${os_short}::default_repos")
+  $default_default_repos = getvar("yum::os::${os_short}::default_repos")
   $default_repos_hiera = str2bool($default_repos_merge)? {
     false   => $default_repos,
     default => hiera_hash('yum::default_repos', {} ),
@@ -67,7 +67,7 @@ class yum (
   $default_repos_real = merge($default_default_repos, $default_repos_hiera)
 
   # merge together all extra_repos sources
-  $default_extra_repos = getvar("::yum::os::${os_short}::extra_repos")
+  $default_extra_repos = getvar("yum::os::${os_short}::extra_repos")
   $extra_repos_hiera = str2bool($extra_repos_merge)? {
     false   => $extra_repos,
     default => hiera_hash('yum::extra_repos', {} ),
@@ -75,7 +75,7 @@ class yum (
   $extra_repos_real = merge($default_extra_repos, $extra_repos_hiera)
 
   # merge together all options sources
-  $default_options = getvar("::yum::os::${os_short}::options")
+  $default_options = getvar("yum::os::${os_short}::options")
   $options_hiera = str2bool($options_merge)? {
     false   => $options,
     default => hiera_hash('yum::options', {} ),
@@ -83,7 +83,7 @@ class yum (
   $options_real = merge($default_options, $options_hiera)
 
   # merge together all plugins sources
-  $default_plugins = getvar("::yum::os::${os_short}::plugins")
+  $default_plugins = getvar("yum::os::${os_short}::plugins")
   $plugins_hiera = str2bool($plugins_merge)? {
     false   => $plugins,
     default => hiera_hash('yum::plugins', {} ),
