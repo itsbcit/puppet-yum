b091d8ddf6f01e004f5af6cb2655e81a8a55bac1
Jesse Weisner <jesse@weisner.ca>
2014-11-02 22:37:36 -0800

    fixed fat arrows

--- manifests/os/centos6.pp
+++ manifests/os/centos6.pp
@@ -5,14 +5,14 @@ class yum::os::centos6{
   $centos_debug_file = '/etc/yum.repos.d/CentOS-Debug.repo'
   $default_yum_repos = {
     # CentOS-Base.repo
-    'base'       = { 'enabled' => '1', 'path' => $centos_base_file },
-    'centosplus' = { 'enabled' => '0', 'path' => $centos_base_file },
-    'contrib'    = { 'enabled' => '1', 'path' => $centos_base_file },
-    'extras'     = { 'enabled' => '1', 'path' => $centos_base_file },
-    'updates'    = { 'enabled' => '1', 'path' => $centos_base_file },
+    'base'       => { 'enabled' => '1', 'path' => $centos_base_file },
+    'centosplus' => { 'enabled' => '0', 'path' => $centos_base_file },
+    'contrib'    => { 'enabled' => '1', 'path' => $centos_base_file },
+    'extras'     => { 'enabled' => '1', 'path' => $centos_base_file },
+    'updates'    => { 'enabled' => '1', 'path' => $centos_base_file },
 
     # CentOS-Debuginfo.repo
-    'debug'      = { 'enabled' => '0', 'path' => $centos_debug_file },
+    'debug'      => { 'enabled' => '0', 'path' => $centos_debug_file },
   }
 
   $options = {
